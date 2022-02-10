9223e830c74efdbacfa6cd383d4f6d13dc36d11a
Generalsoi <ikhinobelesuccess@gmail.com>
2022-02-10 23:49:35 +0100

    started working on the portfolio site

---
+++ src/Homepage/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Homepage.css";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="introduction">
+        <h3>
+          Success <br /> Ikhinobele
+        </h3>
+        <p>Software Developer</p>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
